ef0f1a055858c12e18eeb73fee69a83799183c05
Sam Tran <quocsam93@gmail.com>
2024-01-17 11:24:09 +0700

    Preparing to add Grant codes into the project

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(name='src', 
-        packages=find_packages())
+        packages=find_packages(),
+        version='0.1.0',
+      
+        extras_require={
+                'phase-label-network': ['tensorflow==2.13', 'keras', 'seaborn']
+        }
+      )
